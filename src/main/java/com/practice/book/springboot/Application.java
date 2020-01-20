25281fd00e47a0fed5a34198edc1bea9e942b94e
YoonSung Kim <opqudk@gmail.com>
2020-01-20 15:45:16 +0900

    게시글 등록기능 추가

--- src/main/java/com/practice/book/springboot/Application.java
+++ src/main/java/com/practice/book/springboot/Application.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-    public static void mina(String[] args) { SpringApplication.run(Application.class, args); }
+    public static void main(String[] args) { SpringApplication.run(Application.class, args); }
 }
